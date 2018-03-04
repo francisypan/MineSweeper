6c67fdb935754de103094d76ba4ebd5abf4d05be
Francis Pan <francisypan@gmail.com>
2018-03-03 16:09:46 -0800

    board functionality all done, don't have mine click handling yet

--- MineSweeper.py
+++ MineSweeper.py
@@ -23,8 +23,8 @@ import random
 # ///////////////////////////////////////////////////// GLOBALS //
 # ////////////////////////////////////////////////////////////////
 
-gameSize = 10
-numBombs = 10
+gameSize = 30
+numBombs = 100
 # ////////////////////////////////////////////////////////////////
 # //////////////////////////////////// DECLARE APP, MAINSCREEN, //
 # ///////////////////// ACTOR CLASSES/METHODS AND SCREENMANAGER //
@@ -42,57 +42,125 @@ Window.system_size = (gameSize * 25, gameSize * 25)
 class MainScreen(Screen):
     pass
 
+#/////////////////////////////////////////////////////// METHODS //
+
+def addBombs():
+	global numBombs
+	global tileStates
+	while numBombs > 0:
+		addBomb()
+		numBombs -= 1
+
+def addBomb():
+	global tileStates
+	global gameBoard
+	randX = random.randint(-1, gameSize - 1)
+	randY = random.randint(-1, gameSize - 1)
+	if (tileStates[randX][randY] == 0):
+        	global gameBoard
+        	tileStates[randX][randY] = -1
+		gameBoard[randX][randY].tileStatus = -1
+	else:
+		addBomb()
+
+def fillBoard():
+	global tileStates
+	for x in range(gameSize):
+		for y in range(gameSize):
+			if (tileStates[x][y] == -1):
+				trySetTile(x - 1, y - 1)
+				trySetTile(x - 1, y)
+				trySetTile(x - 1, y + 1)
+				trySetTile(x, y - 1)
+				trySetTile(x, y + 1)
+				trySetTile(x + 1, y - 1)
+				trySetTile(x + 1, y)
+				trySetTile(x + 1, y + 1)
+
+def trySetTile(x, y):
+	global tileStates
+	global gameBoard
+	if (x > gameSize - 1 or x < 0 or y > gameSize - 1 or y < 0 or tileStates[x][y] == -1):
+		return
+	else:
+		tileStates[x][y] += 1
+		gameBoard[x][y].tileStatus = tileStates[x][y]
+
+def checkNeighbors(pos):
+	x, y = pos
+	tileCheck(x - 1, y - 1)
+	tileCheck(x - 1, y)
+	tileCheck(x - 1, y + 1)
+	tileCheck(x, y - 1)
+	tileCheck(x, y + 1)
+	tileCheck(x + 1, y - 1)
+	tileCheck(x + 1, y)
+	tileCheck(x + 1, y + 1)
+
+def tileCheck(x, y):
+	global gameBoard
+	global tileStates
+	if (x > gameSize - 1 or x < 0 or y > gameSize - 1 or y < 0 or tileStates[x][y] == -1 or gameBoard[x][y].revealed == True):
+		return
+	elif (tileStates[x][y] == 0):
+		gameBoard[x][y].source = str(tileStates[x][y]) + '.jpg'
+		gameBoard[x][y].revealed = True
+		pos = x, y
+		checkNeighbors(pos)
+	else:
+		gameBoard[x][y].source = str(tileStates[x][y]) + '.jpg'
+		gameBoard[x][y].revealed = True
+
+def revealAll():
+	global gameBoard
+	global tileStates
+	for x in range(gameSize):
+		for y in range(gameSize):
+			gameBoard[x][y].reveal(tileStates[x][y])
+
 #////////////////////////////////////////////// GAME TILE CLASS //
 class GameTile(ButtonBehavior, Image):
     def __init__(self, **kwargs):
-        super(GameTile, self).__init__(**kwargs)
-        self.flagged = False
-        self.mine = False
-        self.allow_stretch = True
-        self.background_color = 0, 0, 0, 0
-        self.background_normal = ''
-        self.source = 'default.jpg'
+		super(GameTile, self).__init__(**kwargs)
+		self.revealed = False
+		self.flagged = False
+		self.tileStatus = 0
+		self.tilePos = 0, 0
+		self.allow_stretch = True
+		self.background_color = 0, 0, 0, 0
+		self.background_normal = ''
+		self.source = 'default.jpg'
 
     def on_touch_down(self, touch):
         if self.collide_point(touch.x, touch.y):
-            if (touch.button == 'right' and self.flagged == False):
+            if (touch.button == 'right' and self.flagged == False and self.revealed == False):
                 self.source = 'flagged.jpg'
                 self.flagged = True
-            elif (touch.button == 'right' and self.flagged == True):
+            elif (touch.button == 'right' and self.flagged == True and self.revealed == False):
                 self.source = 'default.jpg'
                 self.flagged = False
-            elif (touch.button == 'left' and self.flagged == False):
-                self.reveal()
+            elif (touch.button == 'left' and self.flagged == False and self.revealed == False):
+                self.reveal(self.tileStatus)
 
-    def reveal(self):
-        if self.mine == True:
-            self.source = 'mine.jpg'
-        elif self.mine == False:
-            self.source = '0.jpg'
-
-    def checkNeighbors(self):
-        pass
+    def reveal(self, tileStatus):
+		self.source = str(tileStatus) + '.jpg'
+		self.revealed = True
+		if (tileStatus == 0):
+			checkNeighbors(self.tilePos)
 
 #////////////////////////////////////// INITIALLIZES MINE FIELD //
-gameBoard = [[GameTile() for y in range(gameSize)] for x in range(gameSize)]
-tileState = [[0 for y in range(gameSize)] for x in range(gameSize)]
-while numBombs >= 0:
-    randomX = random.randint(-1, 9)
-    randomY = random.randint(-1, 9)
-    if (tileState[randomX][randomY] == 0):
-        global gameBoard
-        tileState[randomX][randomY] == -1
-        tempTile = gameBoard[randomX][randomY]
-        tempTile.mine = True
-    numBombs -= 1
 
+gameBoard = [[GameTile() for y in range(gameSize)] for x in range(gameSize)]
+tileStates = [[0 for y in range(gameSize)] for x in range(gameSize)]
+addBombs()
+fillBoard()
+#revealAll()
 # ////////////////////////////////////////////////////////////////
 # ////////////////////////////////////// CREATE GRID AND ACTORS //
 # ////////////////////////////////////////////////////////////////
 
 grid = GridLayout(
     id = 'grid',
-    color = [0.8, 0.8, 0.8, 1],
     cols = gameSize,
     rows = gameSize,
     padding = 0,
@@ -104,14 +172,13 @@ grid = GridLayout(
 
 for x in range (grid.cols):
     for y in range (grid.rows):
-        global gameBoard
-        grid.add_widget(gameBoard[x][y])
+		global gameBoard
+		gameBoard[x][y].tilePos = x, y
+		grid.add_widget(gameBoard[x][y])
 
 main = MainScreen(name = 'main')
 main.add_widget(grid)
 sm.add_widget(main)
-
-
 
 # ////////////////////////////////////////////////////////////////
 # ///////////////////////////////////////////////////// RUN APP //
